4b218de2be169c10fdde8b3201b3e0c87cb8177f
kmu-dos <94980571+kmu-dos@users.noreply.github.com>
2021-12-20 14:27:10 +0900

    12/20 update
    - 표 추가
    - input에 값 추가 안해도 default 값이 전달되게끔
    - 텍스트 수정

--- demo.js
+++ demo.js
@@ -1,9 +1,54 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const chart = document.getElementById('data-graph').getContext('2d');
+    let resultChart = new Chart(chart, {
+        type: 'bar',
+        data: {
+            labels: ['A', 'B'],
+            datasets: [{
+                label: 'latency (m/s)',
+                data: [1000, 3000],
+                backgroundColor : '#4CAAE8',
+            }]
+        },
+        options: {
+            plugins : {
+                legend: {
+                    display: false,
+                }
+            },
+            responsive:true,
+            maintainAspectRatio: false,
+            scales: {
+                x: {
+                    type: 'linear',
+                    grid: {
+                        display: false
+                    },
+                },
+                y: {
+                    type: 'linear',
+                    position: 'left',
+                    scalePositionLeft: true,
+                    title: {
+                        display: true,
+                        text: 'Latency (ms)',
+                        font : {
+                            size : 14,
+                        }
+                    },
+                }
+            }
+        },
+    })
+    console.log('hi');
+})
+
 async function submit (){
-    const lr = document.getElementById('nr_l').value;
-    const lc = document.getElementById('nc_l').value;
-    const rc = document.getElementById('nc_r').value;
-    const ld = document.getElementById('d_l').value;
-    const rd = document.getElementById('d_r').value;
+    const lr = document.getElementById('nr_l').value ? document.getElementById('nr_l').value : 10000;
+    const lc = document.getElementById('nc_l').value ? document.getElementById('nc_l').value : 60000;
+    const rc = document.getElementById('nc_r').value ? document.getElementById('nc_r').value : 20000;
+    const ld = document.getElementById('d_l').value ? document.getElementById('d_l').value : 0.0001;
+    const rd = document.getElementById('d_r').value ? document.getElementById('d_r').value : 0.03;
     const lnnz = lr*lc*ld;
     const rnnz = lc*rc*rd;
     let resultData;
