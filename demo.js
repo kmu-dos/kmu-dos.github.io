87cc5ffd48b10afd693150bc50d499fae9a9ed68
kmu-dos <94980571+kmu-dos@users.noreply.github.com>
2021-12-31 20:34:11 +0900

    12/31 update
    10초 이상 api data 받아오지 못할 시 , 요청 취소기능
    전 페이지 title 변경

--- demo.js
+++ demo.js
@@ -74,8 +74,15 @@ async function submit (){
     const lnnz = lr*lc*ld;
     const rnnz = lc*rc*rd;
     let resultData;
-    let timeOut = 0;
     const api = "https://lcukdaf75g.execute-api.us-west-2.amazonaws.com/dos-inference-stage/dos-inference-resource";
+    const timeout = 10000;
+    const controller = new AbortController();
+    const id = setTimeout(() => {
+        // 취소 callback
+        load.style.display = 'none';
+        alert("Timeout error. \nPlease contact the site administrator at bigdata@kookmin.ac.kr");
+        controller.abort();
+    }, timeout);
     await fetch(api, {
         method: "POST",
         headers: {
@@ -90,85 +97,85 @@ async function submit (){
             "lnnz" : lnnz,
             "rnnz" : rnnz
         }),
+        signal: controller.signal,
     }).then((result) => result.json())
         .then((data) => {
             resultData = data.body;
             load.style.display = 'none';
-    }).catch((e) => console.log(e));
-
-
-    const sparse = parseInt(resultData.split("sm*sm : [")[1].split(']')[0]);
-    const dense = parseInt(resultData.split('sm*dm : [')[1].split(']')[0]);
-    const optimal = resultData.split('optim_method : ')[1].split('\"')[0];
-    let x = optimal==='smsm' ? dense/sparse : sparse/dense;
-    x = x.toFixed(2);
-    document.getElementById('sparse').innerText = sparse;
-    document.getElementById('dense').innerText =dense;
-    document.getElementById('optim_method').innerText = x;
-    document.getElementById('result-comment').innerText = optimal==='smsm' ?
-        ("The performance of Sparse X Sparse is " + x + " better than the default Apache Spark SPMM.")
-        : ("The performance of default Apache Spark SPMM is improved " + x + " times than Sparse X Sparse.");
-    document.getElementById('result-comment').style.color = optimal==='smsm' ? 'red' : '#000';
-    if(typeof resultChart != 'undefined'){
-        resultChart.destroy()
-    }
-    resultChart = new Chart(chart, {
-        type: 'bar',
-        data: {
-            labels: ['Sparse X Sparse', 'Sparse X Dense'],
-            datasets: [{
-                label: 'Latency (ms)',
-                backgroundColor: "#4c9de7",
-                data: [sparse, dense],
-                barThickness: 50,
-                maxBarThickness: 80,
-            }]
-        },
-        options: {
-            responsive:true,
-            maintainAspectRatio: false,
-            plugins: {
-                legend: {
-                    display: false,
-                },
-            },
-            scales: {
-                x: {
-                    grid : {
-                        display : false,
-                    },
-                    ticks: {
-                        font: {
-                            family : 'Roboto',
-                            size: 14
-                        },
-                    }
-                },
-                y: {
-                    type: 'linear',
-                    position: 'left',
-                    scalePositionLeft: true,
-                    title: {
-                        display: true,
-                        text: 'Latency (ms)',
-                        font : {
-                            family : 'Roboto',
-                            size : 18,
-                            weight : 'bold',
-                        },
-                        color: '#424242',
-                    },
-                    beginAtZero : true,
-                    ticks : {
-                        fontSize:12,
-                        padding: 10,
-                    },
-                    grid: {
-                        display: true,
-                    }
-                }
+            const sparse = parseInt(resultData.split("sm*sm : [")[1].split(']')[0]);
+            const dense = parseInt(resultData.split('sm*dm : [')[1].split(']')[0]);
+            const optimal = resultData.split('optim_method : ')[1].split('\"')[0];
+            let x = optimal==='smsm' ? dense/sparse : sparse/dense;
+            x = x.toFixed(2);
+            document.getElementById('sparse').innerText = sparse;
+            document.getElementById('dense').innerText =dense;
+            document.getElementById('optim_method').innerText = x;
+            document.getElementById('result-comment').innerText = optimal==='smsm' ?
+                ("The performance of Sparse X Sparse is " + x + " better than the default Apache Spark SPMM.")
+                : ("The performance of default Apache Spark SPMM is improved " + x + " times than Sparse X Sparse.");
+            document.getElementById('result-comment').style.color = optimal==='smsm' ? 'red' : '#000';
+            if(typeof resultChart != 'undefined'){
+                resultChart.destroy()
             }
+            resultChart = new Chart(chart, {
+                type: 'bar',
+                data: {
+                    labels: ['Sparse X Sparse', 'Sparse X Dense'],
+                    datasets: [{
+                        label: 'Latency (ms)',
+                        backgroundColor: "#4c9de7",
+                        data: [sparse, dense],
+                        barThickness: 50,
+                        maxBarThickness: 80,
+                    }]
+                },
+                options: {
+                    responsive:true,
+                    maintainAspectRatio: false,
+                    plugins: {
+                        legend: {
+                            display: false,
+                        },
+                    },
+                    scales: {
+                        x: {
+                            grid : {
+                                display : false,
+                            },
+                            ticks: {
+                                font: {
+                                    family : 'Roboto',
+                                    size: 14
+                                },
+                            }
+                        },
+                        y: {
+                            type: 'linear',
+                            position: 'left',
+                            scalePositionLeft: true,
+                            title: {
+                                display: true,
+                                text: 'Latency (ms)',
+                                font : {
+                                    family : 'Roboto',
+                                    size : 18,
+                                    weight : 'bold',
+                                },
+                                color: '#424242',
+                            },
+                            beginAtZero : true,
+                            ticks : {
+                                fontSize:12,
+                                padding: 10,
+                            },
+                            grid: {
+                                display: true,
+                            }
+                        }
+                    }
 
-        }
-    })
+                }
+            })
+    }).catch((e) => console.log(e));
+    clearTimeout(id);
 }
