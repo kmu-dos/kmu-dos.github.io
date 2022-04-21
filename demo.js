570bf458ce323269aa6b73c3bfbc0fbdb58efc69
kmu-dos <94980571+kmu-dos@users.noreply.github.com>
2022-04-21 17:06:57 +0900

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,12 +1,12 @@
 window.onload = function() {
-  const lr = document.getElementById('nr_l').value ? document.getElementById('nr_l').value : 10000;
-  const lc = document.getElementById('nc_l').value ? document.getElementById('nc_l').value : 60000;
-  const rc = document.getElementById('nc_r').value ? document.getElementById('nc_r').value : 20000;
-  const ld = document.getElementById('d_l').value ? document.getElementById('d_l').value : 0.0001;
-  const rd = document.getElementById('d_r').value ? document.getElementById('d_r').value : 0.03;
+  const lr = 10000;
+  const lc = 60000;
+  const rc = 20000;
+  const ld = 0.0001;
+  const rd = 0.03;
   const lnnz = lr*lc*ld;
-  const rnnz = lc*rc*rd;    
-	let resultData;
+  const rnnz = lc*rc*rd;
+  let resultData;
   const api = "https://lcukdaf75g.execute-api.us-west-2.amazonaws.com/dos-inference-stage/dos-inference-resource";
 
 	fetch(api, {
@@ -23,7 +23,7 @@ window.onload = function() {
             "lnnz" : lnnz,
             "rnnz" : rnnz
         }),
-  }).then((response) => console.log(response));
+  }).then((response) => console.log(""));
 };
 
 let chart, resultChart
